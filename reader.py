c9727f537b44678b4365def9f84af785a4e6d0ce
alopacin <adlopacinski@gmail.com>
2023-08-14 15:55:33 +0200

    zapisanie pracy

--- reader.py
+++ reader.py
@@ -28,16 +28,48 @@ class BaseReader:
 
 class CSVReader(BaseReader):
     def read(self, file):
-        with open(file_in, 'r') as f:
+        with open(file, 'r') as f:
             reader = csv.reader(f)
             for line in reader:
                 data.append(line)
 
     def write(self, file):
-        with open(file_out, 'w', newline='') as f:
+        with open(file, 'w', newline='') as f:
             writer = csv.writer(f)
             for line in data:
                 writer.writerow(line)
+
+
+class JSONReader(BaseReader):
+    def read(self, file):
+        with open(file, 'r') as f:
+            return json.load(f)
+
+    def write(self, file):
+        with open(file, 'w') as f:
+            json.dump(data, f)
+
+
+class TXTReader(BaseReader):
+    def read(self, file):
+        with open(file, 'r') as f:
+            for line in f.readlines():
+                return line.strip().split(',')
+
+    def write(self, file):
+        with open(file, 'w') as f:
+            for line in data:
+                f.write(','.join(line) + '\n')
+
+
+class PickleReader(BaseReader):
+    def read(self, file):
+        with open(file, 'rb') as f:
+            return pickle.load(f)
+
+    def write(self, file):
+        with open(file, 'wb') as f:
+            pickle.dump(data, f)
 
 
 
@@ -53,20 +85,5 @@ changes = sys.argv[3:]
 
 data = []
 
-# czesc odpowiadajaca za wyszukanie podanych wspolrzednych i zamianie wartosci
-for change in changes:
-    if len(change.split(',')) != 3:
-        print('Podałeś błędne dane.Spróbuj jeszcze raz')
-        exit()
-    x, y, value = change.split(',')
-    if not x.isdigit() or not y.isdigit():
-        print('Podałeś nieprawidłowe wartości współrzędnych.Spróbuj jeszcze raz')
-        exit()
-    else:
-        x = int(x)
-        y = int(y)
-    if x >= len(data[0]) or y >= len(data):
-        print('Podałeś za duże wartości współrzędnych')
-        exit()
-    data[x][y] = value
+
 print(f'Zmodyfikowany plik będzie miał następującą zawartość: \n{data}')
